8ff2543df8fed69579977f919f6476dc1f1767a2
Kajalben <kajalsavani191@gmail.com>
2021-02-15 16:42:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import './styles/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
